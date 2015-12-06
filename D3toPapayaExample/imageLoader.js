f4904bec2e18fbb6d2d4f23695acfa7cced19a26
dpwalter7 <dpwalter7@gmail.com>
2015-12-06 12:06:46 -0500

    second commit

---
+++ D3toPapayaExample/imageLoader.js
@@ -0,0 +1,8 @@
+var params = [];
+params["worldSpace"] = true;
+params["images"] = ["10002K_INSP_STD_BWH_COPD_Iso1.5mm.nii.gz"];
+// params["myOverlay.nii.gz"] = {"min": 4, "max": 8, "lut": "Red Overlay"};
+//params["myOverlayImage.nii.gz"] = {"min": 4, "max": 10};
+// for more options, see http://ric.uthscsa.edu/mango/papaya_devguide.html
+
+
---
+++ D3toPapayaExample/js/imageLoader.js
@@ -0,0 +1,5 @@
+var params = [];
+params["worldSpace"] = true;
+params["images"] = ["data/myBaseImage.nii.gz", "data/myOverlayImage.nii.gz"];
+params["myOverlayImage.nii.gz"] = {"min": 4, "max": 10};
+// for more options, see http://ric.uthscsa.edu/mango/papaya_devguide.html
